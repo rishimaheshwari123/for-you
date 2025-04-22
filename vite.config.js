da09953a1dc5df7e22a332d3a4c275eb27f1f8bf
Rishi <rishimaheshwari040@gmail.com>
2025-04-22 11:37:32 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss()
   ],
+  server: {
+    host: '0.0.0.0', // or use '13.53.158.250' if binding to a specific IP
+    port: 5173
+  }
 })
